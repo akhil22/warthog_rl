115c777052b1cb4d98f888adfbfdf4d13b3b5d43
Akhil <akhil.nagariya@gmail.com>
2020-12-16 22:53:24 -0600

    added rendering and reward

--- env/WarthogEnv.py
+++ env/WarthogEnv.py
@@ -1,25 +1,57 @@
 from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
 import gym
 import numpy as np
 import math
 from gym import spaces
 import csv
+
+
 class WarthogEnv(gym.Env):
     def __init__(self, waypoint_file):
         super(WarthogEnv, self).__init__()
-        self.action_space = spaces.Box(low = np.array([0.0,-1.]), high = np.array([0.,1.]), shape=(2,))
-        self.observation_space = spaces.Box(low = -100, high = 1000, shape=(42,),dtype = np.float)
+        self.action_space = spaces.Box(low=np.array([0.0, -1.]),
+                                       high=np.array([0., 1.]),
+                                       shape=(2, ))
+        self.observation_space = spaces.Box(low=-100,
+                                            high=1000,
+                                            shape=(42, ),
+                                            dtype=np.float)
         self.filename = waypoint_file
+        plt.ion
         self.waypoints_list = []
         self.num_waypoints = 0
         self.pose = [0, 0, 0]
         self.twist = [0, 0]
         self.closest_idx = 0
+        self.prev_closest_idx = 0
         self.closest_dist = math.inf
         self.num_waypoints = 0
         self.horizon = 10
         self.dt = 0.03
+        self.ref_vel = []
         self._read_waypoint_file(self.filename)
+        self.max_vel = 1
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.warthog_width = 0.5 / 2.0
+        self.warthog_length = 1.0 / 2.0
+        self.warthog_diag = math.sqrt(self.warthog_width**2 +
+                                      self.warthog_length**2)
+        self.plot_waypoints()
+        self.rect = Rectangle((0., 0.), self.warthog_width,
+                              self.warthog_length)
+        self.diag_ang = math.atan2(self.warthog_width, self.warthog_length)
+        ax.add_artist(self.rect)
+
+    def plot_waypoints(self):
+        x = []
+        y = []
+        for i in range(0, self.num_waypoints):
+            x.append(self.waypoints_list[i][0])
+            y.append(self.waypoints_list[i][1])
+        self.ax.plot(x, y, '+g')
+
     def sim_warthog(self, v, w):
         x = self.pose[0]
         y = self.pose[1]
@@ -27,43 +59,49 @@ class WarthogEnv(gym.Env):
         v_ = self.twist[0]
         w_ = self.twist[1]
         dt = self.dt
-        self.pose[0] = x + v_*math.cos(th)*dt
-        self.pose[1] = y + v_*math.sin(th)*dt
-        self.pose[2] = th + w*dt
+        self.pose[0] = x + v_ * math.cos(th) * dt
+        self.pose[1] = y + v_ * math.sin(th) * dt
+        self.pose[2] = th + w * dt
         self.twist[0] = v
-        self.twist[1]= w
+        self.twist[1] = w
+
     def zero_to_2pi(self, theta):
         if theta < 0:
-            theta = 2*math.pi + theta
-        elif theta > 2*math.pi:
-            theta = theta - 2*math.pi
+            theta = 2 * math.pi + theta
+        elif theta > 2 * math.pi:
+            theta = theta - 2 * math.pi
         return theta
+
     def pi_to_pi(self, theta):
         if theta < -math.pi:
-            theta = theta + 2*math.pi
+            theta = theta + 2 * math.pi
         elif theta > math.pi:
-            theta = theta - 2*math.pi
+            theta = theta - 2 * math.pi
         return theta
+
     def get_dist(self, waypoint, pose):
         xdiff = pose[0] - waypoint[0]
         ydiff = pose[1] - waypoint[1]
-        return math.sqrt(xdiff*xdiff + ydiff*ydiff)
+        return math.sqrt(xdiff * xdiff + ydiff * ydiff)
+
     def update_closest_idx(self, pose):
         idx = self.closest_idx
         self.closest_dist = math.inf
         for i in range(self.closest_idx, self.num_waypoints):
             dist = self.get_dist(self.waypoints_list[i], pose)
-            if(dist <= self.closest_dist):
+            if (dist <= self.closest_dist):
                 self.closest_dist = dist
                 idx = i
             else:
                 break
         self.closest_idx = idx
+
     def get_theta(self, xdiff, ydiff):
         theta = math.atan2(ydiff, xdiff)
         return self.zero_to_2pi(theta)
+
     def get_observation(self):
-        obs = [0]*(self.horizon*4 + 2)
+        obs = [0] * (self.horizon * 4 + 2)
         pose = self.pose
         twist = self.twist
         self.update_closest_idx(pose)
@@ -78,38 +116,79 @@ class WarthogEnv(gym.Env):
                 vehicle_th = self.zero_to_2pi(pose[2])
                 #vehicle_th = -vehicle_th
                 #vehicle_th = 2*math.pi - vehicle_th
-                yaw_error = self.pi_to_pi(self.waypoints_list[k][2] - vehicle_th)
+                yaw_error = self.pi_to_pi(self.waypoints_list[k][2] -
+                                          vehicle_th)
                 vel = self.waypoints_list[k][3]
                 obs[j] = r
-                obs[j+1] = self.pi_to_pi(th - vehicle_th)
-                obs[j+2] = yaw_error
-                obs[j+3] = vel - twist[0]
+                obs[j + 1] = self.pi_to_pi(th - vehicle_th)
+                obs[j + 2] = yaw_error
+                obs[j + 3] = vel - twist[0]
             else:
                 obs[j] = 0.
-                obs[j+1] = 0.
-                obs[j+2] = 0.
-                obs[j+3] = 0.
-            j = j+4
+                obs[j + 1] = 0.
+                obs[j + 2] = 0.
+                obs[j + 3] = 0.
+            j = j + 4
         obs[j] = twist[0]
-        obs[j+1] = twist[1]
+        obs[j + 1] = twist[1]
         return obs
+
     def step(self, action):
         self.sim_warthog(action[0], action[1])
+        self.prev_closest_idx = self.closest_idx
         obs = self.get_observation()
         done = False
         if self.closest_idx >= self.num_waypoints - 1:
             done = True
-        return obs, 0, done, {}
+        #Calculating reward
+        k = self.closest_idx
+        xdiff = self.waypoints_list[k][0] - self.pose[0]
+        ydiff = self.waypoints_list[k][1] - self.pose[1]
+        th = self.get_theta(xdiff, ydiff)
+        yaw_error = self.pi_to_pi(th - self.pose[2])
+        phi_error = self.pi_to_pi(self.waypoints_list[self.closest_idx][2] -
+                                  self.pose[2])
+        vel_error = self.waypoints_list[k][3] - self.twist[0]
+        crosstrack_error = self.closest_dist * math.sin(yaw_error)
+        if (math.fabs(crosstrack_error) > 1.5 or math.fabs(phi_error) > 1.4):
+            done = True
+        reward = (2.0 - math.fabs(crosstrack_error)) * (
+            4.0 - math.fabs(vel_error)) * (math.pi / 3. - math.fabs(phi_error))
+        if (self.prev_closest_idx == self.closest_idx
+                or math.fabs(vel_error) > 1.5):
+            reward = 0
+        return obs, reward, done, {}
+
     def reset(self):
+        if (max_vel >= 5):
+            max_vel = 1
         idx = np.random.randint(self.num_waypoints, size=1)
         idx = idx[0]
+        self.closest_idx = 0
+        self.prev_closest_idx = 0
         self.pose[0] = self.waypoints_list[idx][0] + 0.1
-        self.pose[1]= self.waypoints_list[idx][1] + 0.1
+        self.pose[1] = self.waypoints_list[idx][1] + 0.1
         self.pose[2] = self.waypoints_list[idx][2] + 0.01
         self.twist = [0., 0., 0.]
+        for i in range(0, self.num_waypoints):
+            if (ref_vel[i] > max_vel):
+                self.waypoints_list[i][3] = max_vel
+            else:
+                self.waypoints_list[i][3] = ref_vel
+        max_vel = max_vel + 1
         print(self.pose)
-    def render(self, mode='huaman'):
-        pass
+
+    def render(self, mode='human'):
+        total_diag_ang = self.diag_ang + self.pose[2]
+        xl = self.pose[0] - self.warthog_diag * math.cos(total_diag_ang)
+        yl = self.pose[1] - self.warthog_diag * math.sin(total_diag_ang)
+        self.rect.set_xy((xl, yl))
+        self.rect.angle = self.pose[2]
+        self.ax.set_xlim([self.pose[0] - 4, self.pose[0] + 4])
+        self.ax.set_ylim([self.pose[1] - 4, self.pose[1] + 4])
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
     def _read_waypoint_file(self, filename):
         with open(filename) as csv_file:
             pos = csv.reader(csv_file, delimiter=',')
@@ -118,16 +197,27 @@ class WarthogEnv(gym.Env):
                 utm_cord = [float(row[0]), float(row[1])]
                 #phi = math.pi/4
                 phi = 0.
-                xcoord = utm_cord[0]*math.cos(phi) + utm_cord[1]*math.sin(phi)
-                ycoord = -utm_cord[0]*math.sin(phi) + utm_cord[1]*math.cos(phi)
-             #   self.waypoints_list.append(np.array([xcoord, ycoord, float(row[2]),float(row[3])]))
+                xcoord = utm_cord[0] * math.cos(phi) + utm_cord[1] * math.sin(
+                    phi)
+                ycoord = -utm_cord[0] * math.sin(phi) + utm_cord[1] * math.cos(
+                    phi)
+                #   self.waypoints_list.append(np.array([xcoord, ycoord, float(row[2]),float(row[3])]))
                 #self.waypoints_list.append(np.array([xcoord, ycoord, float(row[2]),2.5]))
-                self.waypoints_list.append(np.array([utm_cord[0], utm_cord[1], float(row[2]),float(row[3])]))
-               # self.waypoints_list.append(np.array([utm_cord[0], utm_cord[1], float(row[2]), 1.5]))
+                self.waypoints_list.append(
+                    np.array([
+                        utm_cord[0], utm_cord[1],
+                        float(row[2]),
+                        float(row[3])
+                    ]))
+                self.ref_vel.append(float(row[3]))
+            # self.waypoints_list.append(np.array([utm_cord[0], utm_cord[1], float(row[2]), 1.5]))
             for i in range(0, len(self.waypoints_list) - 1):
-                xdiff = self.waypoints_list[i+1][0] - self.waypoints_list[i][0]
-                ydiff = self.waypoints_list[i+1][1] - self.waypoints_list[i][1]
-                self.waypoints_list[i][2] = self.zero_to_2pi(self.get_theta(xdiff, ydiff))
-            self.waypoints_list[i+1][2] = self.waypoints_list[i][2]
-            self.num_waypoints = i+2
+                xdiff = self.waypoints_list[i +
+                                            1][0] - self.waypoints_list[i][0]
+                ydiff = self.waypoints_list[i +
+                                            1][1] - self.waypoints_list[i][1]
+                self.waypoints_list[i][2] = self.zero_to_2pi(
+                    self.get_theta(xdiff, ydiff))
+            self.waypoints_list[i + 1][2] = self.waypoints_list[i][2]
+            self.num_waypoints = i + 2
         pass
---
+++ env/temp.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+import time
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+x = np.linspace(0., 6 * np.pi, 100)
+y = np.sin(x)
+ax.plot(x, y, 'r-')
+rect = Rectangle((0, 0), 0.5, 0.3)
+ax.add_artist(rect)
+t = []
+for i in range(0, 100):
+    t1 = time.time()
+    fig.canvas.draw()
+    #fig.canvas.flush_events()
+    rect.set_xy((x[i], y[i]))
+    ax.set_xlim([x[i] - 0.5, x[i] + 0.5])
+    ax.set_ylim([y[i] - 0.5, y[i] + 0.5])
+    t2 = time.time()
+    t.append(t2 - t1)
+print(t)
