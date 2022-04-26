1fd7acb6269c3fc391b0d55d6aa76fe455590f6b
akhil <akhil.nagariya@gmail.com>
2022-04-26 11:42:59 -0500

    added sac for ranger

---
+++ ranger_sac_tf.py
@@ -0,0 +1,57 @@
+import gym
+import numpy as np
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines.common import set_global_seeds, make_vec_env
+#from stable_baselines import PPO2
+from stable_baselines import SAC as PPO2
+from env.RangerEnv_vis import RangerEnv
+from matplotlib import pyplot as plt
+
+
+def make_env(env_id, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environments you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = RangerEnv('ranger_waypoints2.csv')
+        env.seed(seed + rank)
+        return env
+
+    set_global_seeds(seed)
+    return _init
+
+
+if __name__ == '__main__':
+    env_id = "CartPole-v1"
+    num_cpu = 1  # Number of processes to use
+    # Create the vectorized environment
+    env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+    #env = RangerEnv('unity_remote.txt')
+    plt.pause(2)
+    fname = './policy/ranger3_sac_weight_action_penalty'
+
+    # Stable Baselines provides you with make_vec_env() helper
+    # which does exactly the previous steps for you:
+    # env = make_vec_env(env_id, n_envs=num_cpu, seed=0)
+    for i in range(1, 30):
+        if i == 0:
+            model = PPO2('MlpPolicy', env, verbose=1)
+            model.learn(total_timesteps=1000000)
+            model.save(f'{fname}0')
+        else:
+            model1 = PPO2('MlpPolicy', env, verbose=1)
+            #for learning uncomment
+            model = PPO2('MlpPolicy', env, verbose=1)
+            # model.load('./first_pytorch_multiplication_reward.zip')
+            model = PPO2.load(f'{fname}{i-1}')
+            model.env = model1.env
+            model.learn(total_timesteps=1000000)
+            model.save(f'{fname}{i}')
+    print("done training")
