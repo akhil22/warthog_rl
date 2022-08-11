6c198e90b6ae5c47f9e8f9bd24577aa7ead27f68
akhil Nagariya <akhil.nagariya@gmail.com>
2022-08-11 06:51:14 -0500

    added training results

--- warthog_manual_ppo_bicycle.py
+++ warthog_manual_ppo_bicycle.py
@@ -43,7 +43,7 @@ def main():
     env = WarthogEnv('sim_remote_waypoint.txt', None)
     #env = WarthogEnv('real_remote_waypoints.txt')
     plt.pause(2)
-    model = torch.load('./temp_policy/tan_h/manaul_ppo_batch_700000.pt')
+    model = torch.load('./temp_policy/tan_h/manaul2_ppo_batch_1541400.pt')
     #model.save('./policy/zero_train')
     #model = PPO2.load('./policy/vel_weight8_stable8')
     #model = PPO2.load('./policy/vel_airsim_test_final_6xfast3')
